3b97f4fbc236cb6a33c35a143771576741902ac1
Rony Chung <bluegreenapple.87@gmail.com>
2015-06-26 11:36:59 -0300

    adicionando tela diagnosticos

---
+++ public/js/controllers/main3.js
@@ -0,0 +1,17 @@
+angular.module('ui.bootstrap.demo', ['ui.bootstrap'])
+
+
+	.controller('TabsDemoCtrl', function ($scope, $window) {
+	  $scope.tabs = [
+	    { title:'Doble', content:'Under Construction...' },
+	    { title:'Iec 60599', content:'Under Construction...' },
+	    { title:'Laborelec', content:'Under Construction...' },
+	    { title:'Pugh', content:'Under Construction...' },
+	    { title:'Iec ', content:'Under Construction...' },
+	    { title:'Rogers', content:'Under Construction...' },
+	    { title:'Duval', content:'Under Construction...' },
+	    { title:'Dornemburg', content:'Under Construction...' },
+	  ];
+
+	
+	});
