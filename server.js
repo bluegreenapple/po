ec0a08c757b47bcf356dea6c54fa802169163a5b
Rony Chung <bluegreenapple.87@gmail.com>
2015-07-06 12:03:33 -0300

    ammend

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ require('./app/routes2.js')(app);
 // 	  res.sendfile('./public/indexAlarmes.html');
 // });
 app.get('/', function (req, res) {
-  res.sendfile('./public/index3.html');
+  res.sendfile('./public/login.html');
 });
 app.get('/login', function (req, res) {
   res.sendfile('./public/indexLogin.html');
