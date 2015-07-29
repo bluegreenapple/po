3021bec7c86fb85e55737f62c7cca338749c9a91
Rony Chung <bluegreenapple.87@gmail.com>
2015-07-29 18:06:58 -0300

    sorting equipamentos by tag

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,7 @@ function getEquipamentos(res){
 			if (err)
 				res.send(err)
 
+			// equipamentos = _.sortBy(equipamentos, function(equipamento){ return equipamento.tag; });
 			res.json(equipamentos); // return all equipamentos in JSON format
 		});
 };
